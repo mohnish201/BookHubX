59c9787f78596b6fe51d28416cfcf055bbb4154f
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-11-21 22:29:16 +0530

    Backend and Frontend Environment setup done

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'Frontend';
+
+  ngOnInit():void{
+    initFlowbite()
+  }
+}
